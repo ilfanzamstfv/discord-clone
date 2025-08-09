8d1d224af982ac5299ab8b25f50f36e415afc32d
unknown <ilfanza212@gmail.com>
2025-08-09 20:00:03 +0700

    update create server

--- components/ServerList/CreateServerForm.tsx
+++ components/ServerList/CreateServerForm.tsx
@@ -37,12 +37,13 @@ export default function CreateServerForm() {
       ref={dialogRef}
     >
       <div className="w-full flex flex-col items-center justify-between py-8 px-6">
-        <h2 className="text-3xl flex font-semibold text-gray-300">
+        <h2 className="text-3xl flex font-semibold text-gray-300 gap-6">
           Create Your Server
           <Link href="/">
             <CloseIcon />
           </Link>
         </h2>
+
         <p className="p-4 text-gray-300 w-md text-center">
           Your server is where you and your friends hang out. Make yours and
           start talking.
@@ -50,10 +51,20 @@ export default function CreateServerForm() {
       </div>
       <form method="dialog" className="flex flex-col space-y-2 px-6">
         <label className="labelTitle text-gray-300" htmlFor="serverName">
-          Server Name
+          SERVER NAME
         </label>
-        <div className="flex items-center bg-gray-100">
-          <span className="text-2xl p-2 text-gray-300">#</span>
+        <div className="flex items-center bg-gray-300 rounded-md">
+          <span className="text-2xl p-2 text-gray-700">#</span>
+          <input
+            type="text"
+            id="serverName"
+            name="serverName"
+            value={formData.serverName}
+            onChange={(e) =>
+              setFormData({ ...formData, serverName: e.target.value })
+            }
+            required
+          />
         </div>
       </form>
     </dialog>
